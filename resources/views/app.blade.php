65c35edc3c675f098d425275845cbbff756c06b2
Joe Oh <joh@geekhive.com>
2018-07-25 17:06:11 -0400

    Started single-page-app (SPA) homepage

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,14 +6,30 @@
 </head>
 <body>
 
-<div class="container">
-	<div id="main" class="row">
-		<!-- Sidebar content -->
-		<div id="sidebar" class="col-md-4">
-			@include('includes.nav')
+<div id="main" class="uk-offcanvas-content">
+	<!-- Header -->
+	<div class="header uk-navbar uk-navbar-container uk-box-shadow-medium-bottom">
+		<div class="uk-navbar-left">
+			<button class="uk-navbar-item uk-button uk-button-default uk-float-left">Filters</button>
+		</div>
+		
+		<div class="uk-navbar-center">
+			<a href="/" class="header__title uk-navbar-item uk-logo">{{ setting('site.title') }}</a>
+		</div>
+		
+		<div class="uk-navbar-right">
+			<a class="uk-navbar-item uk-navbar-toggle uk-button uk-float-right" uk-navbar-toggle-icon href="#" uk-toggle="target: #sidebar-nav">Menu&nbsp;</a>
 		</div>
 	</div>
+	
+	<div class="uk-flex uk-flex-center uk-flex-middle uk-background-muted" uk-height-viewport="offset-top: true">
+		Map goes here
+	</div>
+
+	<!-- Sidebar content -->
+	@include('includes.nav')
 </div>
+
 
 <script src="js/app.js" type="text/javascript"></script>
 </body>
--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,5 +1,13 @@
-<nav id="sidebar-nav">
-	<ul class="nav nav-pills nav-stacked">
-		<li><a href="/">Home</a></li>
-	</ul>
+<div id="sidebar-nav" uk-offcanvas="overlay: true; flip: true">
+	<div class="uk-offcanvas-bar uk-flex uk-flex-column">
+		<button class="uk-offcanvas-close" type="button" uk-close></button>
+		
+		<ul class="uk-nav uk-nav-primary">
+			<li class="uk-parent"><a href="#">About</a></li>
+			<hr>
+			<li class="uk-parent"><a href="#">FAQ</a></li>
+			<hr>
+			<li class="uk-parent"><a href="#">Contact Us</a></li>
+		</ul>
+	</div>
 </nav>
